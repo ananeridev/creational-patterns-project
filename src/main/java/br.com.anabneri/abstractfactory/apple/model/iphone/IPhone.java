15abdbb28ef71c4b3a07bfeb5e5f33154b468d89
anabneri <ananeri32@hotmail.com>
2021-03-26 20:07:28 -0300

    creating IPhone class

--- src/main/java/br.com.anabneri/abstractfactory/apple/model/iphone/IPhone.java
+++ src/main/java/br.com.anabneri/abstractfactory/apple/model/iphone/IPhone.java
@@ -1,1 +1,23 @@
-teste
+public abstract class IPhone {
+    CountryRulesAbstractFactory rules;
+
+    public IPhone(CountryRulesAbstractFactory rules) {
+        this.rules = rules;
+    }
+
+    public abstract void getHardware();
+    
+    public void assemble() {
+        System.out.println("Assembling all the hardwares");
+    }
+
+    public void certificates() {
+        System.out.println("Testing all the hardwares");
+        System.out.println(rules.getCertificates().applyCertification());
+    }
+
+    public void pack() {
+        System.out.println("Packing the devices");
+        System.out.println(rules.getPacking().pack());
+    }
+}
