f92e409192e9f146e48d97ad62bef7bf653ed176
anabneri <ananeri32@hotmail.com>
2021-03-26 20:24:57 -0300

    adding a new class

---
+++ src/main/java/br.com.anabneri/abstractfactory/apple/model/iphone/IPhone11Pro.java
@@ -0,0 +1,15 @@
+public class IPhone11Pro extends IPhone {
+
+	public IPhone11Pro(CountryRulesAbstractFactory rules) {
+		super(rules);
+	}
+
+	@Override
+	public void getHardware() {
+		System.out.println("Hardware list");
+		System.out.println("\t- 6.5in Screen");
+		System.out.println("\t- A13 Chipset");
+		System.out.println("\t- 4Gb RAM");
+		System.out.println("\t- 512Gb Memory");
+	}
+}
